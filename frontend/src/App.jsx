00f07dfbfed329700fba7db0b8ef069bf0bdfa45
aiwatko <oktawia.kata@gmail.com>
2022-05-29 15:17:28 +0100

    Update dependencies and configure router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Question } from './pages/host/Question';
+import { Welcome } from './pages/player/Welcome';
 import { GlobalStyle } from './globalStyles';
 
 const App = () => (
   <>
     <GlobalStyle />
-    <Question />
+    <Router>
+      <Routes>
+        <Route path="/host" element={<Question />} />
+        <Route path="/" element={<Welcome />} />
+      </Routes>
+    </Router>
   </>
 );
 
