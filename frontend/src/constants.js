516b00abba48c0077d368bb48a44a19f9d6f192e
aiwatko <oktawia.kata@gmail.com>
2022-07-19 14:25:57 +0100

    Update github actions workflow

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,7 +4,7 @@ export const GAME = '/game';
 export const QUESTION = '/question';
 export const RANKING = '/ranking';
 
-const WS_BACKEND_URL = 'ws://localhost:5000/game';
+const WS_BACKEND_URL = `${process.env.REACT_APP_BACKEND_URL}/game`;
 export const WS_REGISTRATION = {
   host: `${WS_BACKEND_URL}?type=host`,
   player: `${WS_BACKEND_URL}?type=player`,
